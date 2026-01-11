3c9cd5f4d621e946685a6fcd3066ab76292003e5
sweeppy <104524272+sweeppy@users.noreply.github.com>
2026-01-11 16:59:34 +0300

    Implement simple page

---
+++ frontend/src/components/Input.tsx
@@ -0,0 +1,49 @@
+interface InputProps {
+  label?: string;
+  placeholder?: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  type?: string;
+  className?: string;
+}
+
+
+const Input = ({ label, placeholder = "", value, onChange, type = "text", className }: InputProps) => {
+  return (
+    <div className="flex flex-col w-full max-w-sm">
+      {label && (
+        <label className="mb-2 text-pink-50 font-mono font-semibold text-xs">
+          {label}
+        </label>
+      )}
+      <input
+        type={type}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        className={`
+          text-sm
+          px-4 py-2
+          rounded-lg
+          bg-rose-100
+          text-pink-800
+          placeholder-pink-300
+          font-mono
+          font-semibold
+          focus:outline-none
+          focus:ring-2
+          focus:ring-rose-400
+          focus:ring-offset-1
+          transition
+          duration-200
+          ease-in-out
+          shadow-sm
+          hover:shadow-md
+          ${className}
+        `}
+      />
+    </div>
+  )
+}
+
+export default Input
--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,8 +1,34 @@
+import { useState } from "react";
+import Input from "../components/Input";
+
 const Main = () => {
+
+  const [result, setResult] = useState("Some text");
+  const [searchString, setSearchString] = useState(""); 
+  const [foundString, setFoundString] = useState("");
 
   return (
     <>
-
+      <div className="bg-rose-300 w-screen h-screen flex items-center justify-center">
+        <div className="container min-h-screen flex flex-col gap-y-8 items-center justify-center">
+            <h1 className="text-center text-3xl font-mono font-extrabold text-pink-50">Booyer Moore Seach vizualization</h1> 
+            <p className="text-2xl text-center font-mono font-extrabold text-pink-50">{result}</p>
+            <div className="container w-full flex items-center gap-8">
+              <Input 
+              label = "String for search"
+              placeholder = "Text example..."
+              value = {searchString}
+              onChange={(e) => setSearchString(e.target.value)}
+              />
+              <Input
+              label = "String for search"
+              placeholder = "Text example..."
+              value = {foundString}
+              onChange={(e) => setFoundString(e.target.value)}
+              />
+            </div>
+        </div>
+      </div>
     </>
   )
 }
